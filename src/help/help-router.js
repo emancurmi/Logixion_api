dce842bd40c6851cc57c45ab6a68be70fd6829aa
Eman Curmi <emancurmi@gmail.com>
2020-07-16 22:25:50 -0400

    help added

---
+++ src/help/help-router.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+const HelpService = require('./help-service')
+
+const helpRouter = express.Router()
+
+helpRouter
+    .route('/')
+    .get((req, res, next) => {        
+        HelpService.getAllHelp()
+                .then(help => {
+                    res.json(help)
+                })
+                .catch(next)
+        
+    })
+    
+module.exports = stepsRouter
